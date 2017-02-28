b2fabb1583095002834fee640d297cc7e601bfd4
kmarryo <mario.s.lemke@gmail.com>
2017-02-28 09:42:38 +0100

    Solution for display markers with constructor function

--- js/markers.js
+++ js/markers.js
@@ -1,35 +1,51 @@
 // Interesting places for the map with lat-long coordinates and title to show in the map as markers
+// var markers = {
+//     "townHall": {
+//         name: 'Town hall',
+//         lat: 48.368821,
+//         lng: 10.8965303,
+//         title: 'Rathaus'
+//     },
+//     "sausalitos":
+//     {
+//         name: 'Sausalitos',
+//         lat: 48.36697,
+//         lng: 10.8978486,
+//         title: 'Sausalitos'
+//     },
+//     "kahnfahrt":
+//     {
+//         name: 'kahnfahrt',
+//         lat: 48.37551,
+//         lng: 10.9041863,
+//         title: 'Augsburger Kahnfahrt'
+//     }
+// };
+
+
+
+var NewMarker = function (name, lat, lng, title) {
+    this.name = name;
+    this.lat = lat;
+    this.lng = lng;
+    this.title = title;
+};
+
+
 var markers = ko.observableArray([
-    {
-        name: 'Town hall',
-        lat: 48.368821,
-        lng: 10.8965303,
-        title: 'Rathaus'
-    },
-    {
-        name: 'Sausalitos',
-        lat: 48.36697,
-        lng: 10.8978486,
-        title: 'Sausalitos'
-    },
-    {
-        name: 'kahnfahrt',
-        lat: 48.37551,
-        lng: 10.9041863,
-        title: 'Augsburger Kahnfahrt'
-    }
+    new NewMarker('townHall', 48.368821, 10.8965303, 'Rathaus'),
+    new NewMarker('sausalitos', 48.36697, 10.8978486, 'Sausalitos'),
+    new NewMarker('kahnfahrt', 48.37551, 10.9041863, 'Augsburger Kahnfahrt')
 ]);
 
 
 console.log('markers().length', markers().length);
 
-for(var i = 0; i < markers().length; i++) {
+for (var i = 0; i < markers().length; i++) {
     new google.maps.Marker({
-       position: {lat: markers()[i].lat, lng: markers()[i].lng},
-       map: map,
-       title: markers()[i].title
+        position: {lat: markers()[i].lat, lng: markers()[i].lng},
+        map: map,
+        title: markers()[i].title
     });
 }
-
-console.log('markers()[0]', markers()[0]);
 
