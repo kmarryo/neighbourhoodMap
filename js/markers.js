e997c7382ed0f5c9d12c1aacf9d9ac62483ab390
kmarryo <mario.s.lemke@gmail.com>
2017-03-03 17:44:30 +0100

    new structure with viewModel

--- js/markers.js
+++ js/markers.js
@@ -5,41 +5,43 @@ function initMap() {
         center: {lat: 48.369826, lng: 10.8969703},
         zoom: 15
     });
-    // Constructor function for markers
-    var Pin = function (name, lat, lng, title) {
-        var self = this;
-        this.name = name;
-        this.lat = lat;
-        this.lng = lng;
-        this.title = title;
-        this.marker = new google.maps.Marker({
-            position: {lat: self.lat, lng: self.lng},
-            map: map,
-            title: self.title
-        });
-        // Sets Event listener for clicks when pins are clicked.
-        this.marker.addListener('click', this.toggleAnimation);
-    };
 
-    // Animates marker when it is clicked
-    Pin.prototype.toggleAnimation = function () {
+    function myViewModel() {
         var self = this;
-        self.setAnimation(google.maps.Animation.BOUNCE);
+        // Constructor function for markers
+        var Pin = function (name, lat, lng, title) {
+            var self = this;
+            this.name = name;
+            this.lat = lat;
+            this.lng = lng;
+            this.title = title;
+            self.marker = new google.maps.Marker({
+                position: {lat: self.lat, lng: self.lng},
+                map: map,
+                title: self.title
+            });
+            // Sets Event listener for clicks when pins are clicked.
+            self.marker.addListener('click', self.toggleAnimation);
+        };
+        // Animates marker when it is clicked
+        Pin.prototype.toggleAnimation = function () {
+            var self = this;
+            self.setAnimation(google.maps.Animation.BOUNCE);
 
-        setTimeout(function () {
-            self.setAnimation(null);
-        }, 700);
+            setTimeout(function () {
+                self.setAnimation(null);
+            }, 700);
+        }
+        // Knockout observableArray with position and infos for the markers
+        self.pins = ko.observableArray([
+            new Pin('townHall', 48.368821, 10.8965303, 'Rathaus'),
+            new Pin('sausalitos', 48.36697, 10.8978486, 'Sausalitos'),
+            new Pin('kahnfahrt', 48.37551, 10.9041863, 'Augsburger Kahnfahrt')
+        ]);
     }
-    // Knockout observableArray with position and infos for the markers
-    pins = ko.observableArray([
-        new Pin('townHall', 48.368821, 10.8965303, 'Rathaus'),
-        new Pin('sausalitos', 48.36697, 10.8978486, 'Sausalitos'),
-        new Pin('kahnfahrt', 48.37551, 10.9041863, 'Augsburger Kahnfahrt')
-    ]);
-
 
     console.log('pins', pins);
-    ko.applyBindings(pins);
+    ko.applyBindings(myViewModel());
 
 }
 
