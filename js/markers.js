d3b9b10f8a78a1cdf63b8a121c5d13fd7a8908f9
kmarryo <mario.s.lemke@gmail.com>
2017-03-13 10:23:14 +0100

    Keep structure clean

--- js/markers.js
+++ js/markers.js
@@ -1,25 +1,6 @@
 var map, pins;
-var contentString = '<div id="content">'+
-    '<div id="siteNotice">'+
-    '</div>'+
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-    '<div id="bodyContent">'+
-    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-    'sandstone rock formation in the southern part of the '+
-    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-    'south west of the nearest large town, Alice Springs; 450&#160;km '+
-    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-    'Aboriginal people of the area. It has many springs, waterholes, '+
-    'rock caves and ancient paintings. Uluru is listed as a World '+
-    'Heritage Site.</p>'+
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-    'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-    '(last visited June 22, 2009).</p>'+
-    '</div>'+
-    '</div>';
 
+// Google Maps API function for initializing the map
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 48.369826, lng: 10.8969703},
@@ -31,15 +12,6 @@ function initMap() {
 }
 
 
-console.log('pins', pins);
-
-
-
-
-//##Functions
-/**
- *
- */
 function myViewModel() {
     // Knockout observableArray with position and infos for the markers
     var pins = ko.observableArray([
@@ -53,11 +25,9 @@ function myViewModel() {
     };
 }
 
-//##Objects
 
-/**
- * Pin Object
- */
+// Pin constructor
+
 var Pin = function (name, lat, lng, title, content) {
     var self = this;
     this.name = name;
@@ -90,8 +60,12 @@ Pin.prototype.PinIsClicked = function () {
     }, 700);
 
     // GMaps API infowindow
-    this.infowindow.close(map, this.marker);
+    this.infowindow.close();
+    console.log('close info');
     this.infowindow.open(map, this.marker);
+    console.log('open info');
+
+
 
 }
 
